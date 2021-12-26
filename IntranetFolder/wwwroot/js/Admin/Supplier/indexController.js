6b7e05fd8057a16f75f051f358bf09f654378610
Hong Van <vanhongit@outlook.com>
2021-12-26 17:28:20 +0700

    1

---
+++ IntranetFolder/wwwroot/js/Admin/Supplier/indexController.js
@@ -0,0 +1,65 @@
+﻿function addCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
+var indexController = {
+    init: function () {
+        toastr.options = { // toastr options
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": false,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "2000",
+            "timeOut": "2000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        };
+
+        indexController.registerEvent();
+    },
+
+    registerEvent: function () {
+        // format .numbers
+        $('input.numbers').keyup(function (event) {
+            // Chỉ cho nhập số
+            if (event.which >= 37 && event.which <= 40) return;
+
+            $(this).val(function (index, value) {
+                return addCommas(value);
+            });
+        });
+
+        // Tinh click
+        $('tr .tdVal').click(function () {
+            id = $(this).data('id'); // Tinh id: matinh
+
+            indexController.TdVal_Click(id);
+        });
+        // Tinh click
+    },
+    Load_ThanhPho1Partial: function (id) { // tinh id
+        strUrl = $('#hidStrUrl').val();
+        var url = '/ThanhPho1/ThanhPho1Partial';
+
+        $.get(url, { maTinh: id, strUrl: strUrl }, function (response) {
+            $('#ThanhPho1_Tbl').html(response);
+            $('#ThanhPho1_Tbl').show(500);
+        });
+    },
+    TdVal_Click: function (id) { // tinh id
+        $('#ThanhPho1_Create_Partial').hide(500);
+        $('#ThanhPho1_Edit_Partial').hide(500);
+
+        indexController.Load_ThanhPho1Partial(id); // tinh id
+    }
+};
+indexController.init();
