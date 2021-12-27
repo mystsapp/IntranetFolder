c4e11d3ca9c861130a8b3f5391ce8077da502a0d
Hong Van <vanhongit@outlook.com>
2021-12-27 10:25:01 +0700

    1

--- Common/SD.cs
+++ Common/SD.cs
@@ -7,5 +7,8 @@ namespace Common
     public static class SD
     {
         public const string Intranets = "Intranets";
+        public const string admin = "admin";
+        public const string superadmin = "superadmin";
+        public const string dieuhanh = "operator";
     }
 }
